a4a4ff2e96c37dd920670d770512c2ba9dd4ba21
OZi-13 <o_zi.cdr@mail.ru>
2025-09-17 15:57:32 +0300

    /* fix2 for server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const port = 3000;
 app.use(express.static(resolve(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 
     res.status(200);
 });
