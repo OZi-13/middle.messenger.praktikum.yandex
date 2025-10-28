f0a48a53aac622f92d7abdffc9c47c636a37ba13
OZi-13 <o_zi.cdr@mail.ru>
2025-10-28 11:17:49 +0400

    OneMoreTime CI config fix

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,21 @@ module.exports = {
             parserOptions: {
                 project: null,
                 tsconfigRootDir: null,
+                parser: 'espree',
+            },
+            rules: {
+                '@typescript-eslint/no-unsafe-assignment': 'off',
+                '@typescript-eslint/no-unsafe-argument': 'off',
+                '@typescript-eslint/no-unsafe-return': 'off',
+                '@typescript-eslint/no-unsafe-call': 'off',
+                '@typescript-eslint/no-unsafe-member-access': 'off',
+
+                '@typescript-eslint/require-await': 'off',
+                '@typescript-eslint/restrict-template-expressions': 'off',
+                '@typescript-eslint/no-floating-promises': 'off',
+                '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-var-requires': 'off',
+                'import/no-extraneous-dependencies': 'off',
             },
         },
     ],
